745d066aa908f326280098f86b9b82108335a4fc
Audry <audrywolters@gmail.com>
2019-09-19 14:06:17 -0500

    populate table

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import axios from 'axios';
 
+const Todo = props => (
+    <tr>
+        <td>{props.todo.todo_description}</td>
+        <td>{props.todo.todo_responsible}</td>
+        <td>{props.todo.todo_priority}</td>
+        <td>
+            <Link to={ '/edit/' + props.todo._id }>Edit</Link>
+        </td>
+    </tr>
+);
+
 export default class TodosList extends Component {
     constructor(props) {
         super(props);
-        this.state = {todos: []};
+        this.state = { todos: [] };
+    }
+
+    todoList() {
+        return this.state.todos.map(function(currentTodo, i) {
+            return <Todo todo={currentTodo} key={i} />;
+        })
     }
 
     componentDidMount() {
@@ -18,11 +35,23 @@ export default class TodosList extends Component {
             })
     }
 
-
     render() {
         return (
             <div>
-                <p>Welcome to Todos List Component!</p>
+                <h3>Your Todos:</h3>
+                <table className="table table-striped" style={{ marginTop: 20 }}>
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Responsible</th>
+                            <th>Priority</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.todoList() }
+                    </tbody>
+                </table>
             </div>
         )
     }
